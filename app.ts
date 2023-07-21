c3772d59371d42a96c07c39f7efe49b89e35ca5f
Muhammad Hasan <ryuuosku@gmail.com>
2023-07-21 10:35:51 +0700

    v1.0.1

--- app.ts
+++ app.ts
@@ -1,19 +1,19 @@
-import express from "express"
+import express, {Express, Request, Response} from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
 
 dotenv.config()
 
-const app = express()
+const app: Express = express()
 const port = process.env.PORT ?? "3000"
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
-app.get('/', (req, res) => {
-  res.send(`express js api wilayah ⚡ server ${port}`)
+app.get('/', (req: Request, res: Response) => {
+  res.send(`express typescript api wilayah ⚡ server ${port}`)
 })
 
 app.listen(port, () => console.log(`running ⚡ port: ${port}`))
