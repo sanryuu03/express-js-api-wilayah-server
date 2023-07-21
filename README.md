5a3b6e6502a5c2741522222931ee5c6a013bb02f
Muhammad Hasan <79955819+sanryuu03@users.noreply.github.com>
2023-07-21 10:39:26 +0700

    Revert "v1.0.1 (#1)"
    This reverts commit 613826127090ff42c601886c97cf06a6a0dbaebc.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is a [express.js](https://expressjs.com/) with [`typescript`](https://www.typescriptlang.org/).
+This is a [express.js](https://expressjs.com/) with es6.
--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import express, {Express, Request, Response} from "express"
+import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
 
 dotenv.config()
 
-const app: Express = express()
+const app = express()
 const port = process.env.PORT ?? "3000"
 
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`express typescript api wilayah ⚡ server ${port}`)
+app.get('/', (req, res) => {
+  res.send(`express js api wilayah ⚡ server ${port}`)
 })
 
 app.listen(port, () => console.log(`running ⚡ port: ${port}`))
